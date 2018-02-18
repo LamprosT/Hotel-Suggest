f31de6a4de25f852229895737ac2efe780dbf235
Lampros Tzanetos <latz2000@gmail.com>
2018-02-18 15:03:13 +0200

    Add files via upload

--- JSONWriter.java
+++ JSONWriter.java
@@ -1,3 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelmanagement_beanswork;
+
  
 
 import java.io.IOException;
@@ -324,3 +331,4 @@ public class JSONWriter {
         return this.append(JSONObject.valueToString(object));
     }
 }
+
