2ce02b6726c44368a128e1cb154b8b0013956e7f
Lampros Tzanetos <latz2000@gmail.com>
2018-03-09 00:18:32 +0200

    Add files via upload

---
+++ LocationCoordinates.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelmanagement_beanswork;
+
+public class LocationCoordinates {
+	public double latitude;
+	public double longitude;
+	
+	public LocationCoordinates() {
+		
+	}
+	
+	public LocationCoordinates(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+}
